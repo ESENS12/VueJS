5fbd851326304d390d81c7c4fc843897b0fec6b3
ESENS12 <ghdldid12@gmail.com>
2020-01-17 08:53:39 +0900

    initial commit

---
+++ VanillaJavaScript/README.md
@@ -0,0 +1,5 @@
+# Paint Thing using VanilaJS from Nicolas
+
+
+# References
+- https://www.youtube.com/watch?time_continue=201&v=rE8eZktwyV8&feature=emb_logo
--- vue-layout-playground/src/main.js
+++ vue-layout-playground/src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 
 Vue.config.productionTip = false
+// Vue.loadScript("https://maps.googleapis.com/maps/api/js")
 
 new Vue({
   render: h => h(App),
