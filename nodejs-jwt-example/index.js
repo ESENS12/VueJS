784f0c98a81c647cf67560ae90e49599eed16c53
ESENS12 <ghdldid12@gmail.com>
2020-01-29 18:10:55 +0900

    Change Router

--- nodejs-jwt-example/index.js
+++ nodejs-jwt-example/index.js
@@ -4,6 +4,7 @@ const history = require('connect-history-api-fallback');
 const app = express()
 const port = 3000
 
+app.use(express.static(__dirname + '/public'))
 
 app.get('/', (req, res) => res.send("Hello world"))
 
--- nodejs-jwt-example/routes/api/index.js
+++ nodejs-jwt-example/routes/api/index.js
@@ -4,37 +4,58 @@ var path = require("path")
 let router = express.Router();
 
 let jwt = require("jsonwebtoken");
-let secretObj = require("../../config/jwt");
 
 // vue sample
-// router.get('/', function(req, res, next) { 
-//     res.sendFile(path.join(__dirname, '../../public', 'index.html')); 
+router.get('/', function(req, res, next) { 
+    res.sendFile(path.join(__dirname, '../../public', 'index.html')); 
+});
+
+// router.get("/", function(req,res,next){
+    
+//     //토큰 발행 샘플 
+//   let token = jwt.sign({
+//         email: "foo@example.com",  // 토큰의 내용(payload)
+//         userName : "ESENS",
+//       },
+//         secretObj.secret,    // 비밀키
+//         {
+//             expiresIn: secretObj.expiresin    // 유효 시간은 5분
+//         }
+//     );
+//     const decoded = tokenTest(token);
+
+//     if(decoded){
+//         res.cookie("user", token);
+//         res.send("email : " + decoded.email + "\n UserName : " + decoded.userName);
+    
+//         // res.json({
+//         //     token: token
+//         // })
+//     }else{
+//         res.status(403).send("Key is Not Validate!");
+//     };
+
 // });
 
-router.get("/", function(req,res,next){
-    
-    //토큰 발행 샘플 
-  let token = jwt.sign({
-        email: "foo@example.com"   // 토큰의 내용(payload)
-      },
-        secretObj.secret,    // 비밀키
-        {
-            expiresIn: secretObj.expiresin    // 유효 시간은 5분
-        }
-    )
-
+function tokenTest(token){
     if(typeof token != 'undefined'){
-        res.cookie("user", token);
-        res.json({
-            token: token
-        })
-        var decoded = jwt.verify(token, secretObj.secret);
-        console.log("decoded: " + decoded.email);
-    }else{
-        res.status(403).send("Key is Not Validate!");
-    }
 
-});
+        var decoded = jwt.verify(token, secretObj.secret);
+
+        if(decoded.email && decoded.userName){
+            console.log('email and userName is verify');
+        }
+
+        console.log("Decoded Email: " + decoded.email);
+        console.log("Decoded UserName : " + decoded.userName);
+        return decoded;
+
+    }else{
+        //토큰이 유효하지 않을때 처리
+        return false;
+    }
+}
+
 
 router.use('/test', require('../../routes/api/testToken'));
 
