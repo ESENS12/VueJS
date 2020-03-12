7582013bba7a2b11fc65bbbd45d545a453755c01
ESENS12 <ghdldid12@gmail.com>
2020-03-12 09:31:13 +0900

    Add dataTable(sortable)

--- vue-console-page-sample/src/components/Usage.vue
+++ vue-console-page-sample/src/components/Usage.vue
@@ -214,47 +214,23 @@
             </v-flex>
 
             <!-- top service usage -->
-            <!-- <v-flex md12 sm12 lg12>
+            <v-flex md12 sm12 lg12>
                 <material-card
                     color="red"
                     title="Top Service Usage"
                     class="pt-0"
                 >
-                    <div class="container align-center">
-                        <v-layout class="align-center justify-center">
-                            <v-flex lg8 sm12 md8 pa-0>
-                                <horizontal-bar-chart
-                                    class="mt-3"
-                                    v-if="
-                                        isTopData &&
-                                            start_date &&
-                                            end_date &&
-                                            selectedKey
-                                    "
-                                    v-bind:selectedKey="this.selectedKey"
-                                    v-bind:start_date="this.start_date"
-                                    v-bind:end_date="this.end_date"
-                                    @isnottopdata="top_update"
-                                    :width="450"
-                                    :height="300"
-                                    :styles="total_service_usage"
-                                    :options="optionsForLine"
-                                ></horizontal-bar-chart>
-                                <div v-else>
-                                    <v-layout
-                                        class="justify-center text-center pa-3"
-                                    >
-                                        <v-text class="h3">
-                                            No data has been used for the
-                                            selected period.
-                                        </v-text>
-                                    </v-layout>
-                                </div>
-                            </v-flex>
-                        </v-layout>
+                    <div>
+                        <v-data-table
+                            :headers="headers"
+                            :items="desserts"
+                            :sort-by.sync="sortBy"
+                            :sort-desc.sync="sortDesc"
+                            class="elevation-1"
+                        ></v-data-table>
                     </div>
                 </material-card>
-            </v-flex> -->
+            </v-flex>
         </v-layout>
     </v-container>
 </template>
@@ -274,6 +250,103 @@
     export default {
         name: "Usage",
         data: () => ({
+            sortBy: "fat",
+            sortDesc: false,
+            headers: [
+                {
+                    text: "Dessert (100g serving)",
+                    align: "start",
+                    value: "name"
+                },
+                { text: "Calories", value: "calories" },
+                { text: "Fat (g)", value: "fat" },
+                { text: "Carbs (g)", value: "carbs" },
+                { text: "Protein (g)", value: "protein" },
+                { text: "Iron (%)", value: "iron" }
+            ],
+            desserts: [
+                {
+                    name: "Frozen Yogurt",
+                    calories: 159,
+                    fat: 6.0,
+                    carbs: 24,
+                    protein: 4.0,
+                    iron: "1%"
+                },
+                {
+                    name: "Ice cream sandwich",
+                    calories: 237,
+                    fat: 9.0,
+                    carbs: 37,
+                    protein: 4.3,
+                    iron: "1%"
+                },
+                {
+                    name: "Eclair",
+                    calories: 262,
+                    fat: 16.0,
+                    carbs: 23,
+                    protein: 6.0,
+                    iron: "7%"
+                },
+                {
+                    name: "Cupcake",
+                    calories: 305,
+                    fat: 3.7,
+                    carbs: 67,
+                    protein: 4.3,
+                    iron: "8%"
+                },
+                {
+                    name: "Gingerbread",
+                    calories: 356,
+                    fat: 16.0,
+                    carbs: 49,
+                    protein: 3.9,
+                    iron: "16%"
+                },
+                {
+                    name: "Jelly bean",
+                    calories: 375,
+                    fat: 0.0,
+                    carbs: 94,
+                    protein: 0.0,
+                    iron: "0%"
+                },
+                {
+                    name: "Lollipop",
+                    calories: 392,
+                    fat: 0.2,
+                    carbs: 98,
+                    protein: 0,
+                    iron: "2%"
+                },
+                {
+                    name: "Honeycomb",
+                    calories: 408,
+                    fat: 3.2,
+                    carbs: 87,
+                    protein: 6.5,
+                    iron: "45%"
+                },
+                {
+                    name: "Donut",
+                    calories: 452,
+                    fat: 25.0,
+                    carbs: 51,
+                    protein: 4.9,
+                    iron: "22%"
+                },
+                {
+                    name: "KitKat",
+                    calories: 518,
+                    fat: 26.0,
+                    carbs: 65,
+                    protein: 7,
+                    iron: "6%"
+                }
+            ],
+
             showStartPicker: false,
             showEndPicker: false,
             start_date: null, //조회 시작일
@@ -544,7 +617,7 @@
             },
 
             top_update() {
-                console.log('top_update!');
+                console.log("top_update!");
                 this.isTopData = false;
             },
 
