fba88316d1a8340b64211f67a668840c52eb2a54
ESENS12 <ghdldid12@gmail.com>
2020-03-16 09:08:06 +0900

    Update TIL

--- js-playground/README.md
+++ js-playground/README.md
@@ -1,8 +1,5 @@
-# VanillaJs-i18n-Sample
-- simple sample multi-lang using i18n on JS
+# js-playground
+- Javascript playground(vanilla js, jquery, etc..)
 
 # References
-- i18n (https://www.i18next.com/)
-- sample i18n (https://blog.asamaru.net/2015/09/15/internationalization-i18n-for-javascript-i18next/)
-- vue-i18n-sample (https://github.com/ESENS12/VueJS/tree/master/vue-multi-lang-sample)
-
+- 
--- js-playground/app.js
+++ js-playground/app.js
@@ -1,5 +1,20 @@
 var config = {
-    some_url = "https://localhost.test.com/8888"
+    some_url : "https://localhost.test.com/8888"
 };
 
-config();
+var changePage = function (pageName){
+    console.log('changePage  : ' , pageName);
+    if(pageName === 'firstPage'){
+        console.log("content body (before)" ,document.getElementsByClassName('content_body'));
+        
+        //innerHTMl은 child node로 들어감
+        // document.getElementsByClassName('content_body')[0].innerHTML = "<div>this is innerHtml test</div>";
+        
+        //append는 태그가 그냥 plane으로 들어감,
+        document.getElementsByClassName('content_body')[0].append("<div>this is innerHtml test</div>");
+        console.log("content body (after)" ,document.getElementsByClassName('content_body'));
+        // console.log(document.getElementsByClassName('content_body').innerHtml = "<div>this is innerHtml test</div>");
+    }else{
+
+    }
+}
