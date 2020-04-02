fc4ce23e89d70460dfab61ccc78398d9919518bf
ESENS12 <ghdldid12@gmail.com>
2020-04-02 15:33:33 +0900

    add simple sample node scheduler

--- node-scheduler-sample/app.js
+++ node-scheduler-sample/app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var schedule = require('node-schedule');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -37,5 +38,22 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+var i = 0 ;
+// * 초  * 분  * 시  * 일  * 월  * 연
+var scheduler = schedule.scheduleJob('*/3 * * * * *', function(){
+  // '*/3 * * * * *' -> 3초마다
+  // '* * * * * * ' -> 매초마다
+  // '1-10 * * * * * ' -> 현재 초가 1~10초인경우 실행
+   //function () {....} 
+  //  console.log('5분마다 실행'); 
+   console.log('3초마다 실행 : ' , i);
+   i++; 
+   //작업 취소
+   if(i > 10){
+     scheduler.cancel();
+   }
+  });
+  
 
 module.exports = app;
