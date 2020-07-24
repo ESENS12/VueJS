133d1dd603294332758187f416445d8a19bea799
ESENS12 <ghdldid12@gmail.com>
2020-07-24 12:12:53 +0900

    add file sender example

--- nodejs-file-sender-example/app.js
+++ nodejs-file-sender-example/app.js
@@ -7,7 +7,8 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 const cors = require('cors');
-
+const multer = require('multer');
+var ncp = require('ncp').ncp;
 var app = express();
 
 // view engine setup
@@ -20,10 +21,60 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+function copyDir({ source, destination }) {
+  return new Promise((resolve, reject) => {
+   ncp(source, destination, (err) => {
+    if (err) {
+     console.error('Error while copying folder contents.', err);
+     reject(err);
+     return;
+    }
+    resolve();
+   });
+  });
+ }
+
 // app.use('/', indexRouter);
 // app.use('/users', usersRouter);
 
 app.use(cors());
+
+const fileFilter = (req, file, cb) => {
+  const allowedTypes = ["image/jpeg", "image/jpg", "image/png" , "image/gif"];
+  if(!allowedTypes.includes(file.mimetype)){
+    const error = new Error("Incorrect file");
+    error.code = "INCORRECT_FILETYPE";
+    return cb(error,false)
+  }
+  cb(null,true);
+}
+
+var storage = multer.diskStorage({
+  destination: function (request, file, callback) {
+      callback(null, '/uploads/');
+  },
+  filename: function (request, file, callback) {
+      // console.log(file);
+      callback(null, file.originalname)
+  }
+});
+
+const upload = multer({
+  // dest: './uploads',
+  storage,
+  fileFilter,
+  limits:{
+    fileSize: 5000000
+  }
+  // storage: storage
+})
+
+
+
+app.post('/upload',upload.single('file'),(req,res) =>{
+  console.log("upload body : " , req);
+  res.json({file:'Uploaded file'});
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -41,8 +92,5 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-app.post('/upload',(req,res) =>{
-  res.json({file:'Uploaded file'});
-});
 
 app.listen(5000,()=>console.log("Server listening on port 5000!"));
